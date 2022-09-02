4a693ec2a472a7b9165cda1d75a9021daff03339
portismouth <ozzy.gonzalez@gmail.com>
2022-09-01 22:35:55 -0600

    Product Details, Categories and Seeding More Products

--- BlazorEcommerce/Server/Controllers/ProductsController.cs
+++ BlazorEcommerce/Server/Controllers/ProductsController.cs
@@ -7,18 +7,26 @@ namespace BlazorEcommerce.Server.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly IProductService _productService;
 
-        public ProductsController(DataContext context)
+        public ProductsController(IProductService productService)
         {
-            _context = context;
+            _productService = productService;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return Ok(products);
+            var response = await _productService.GetProductsAsync();
+
+            return Ok(response);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<Product>> GetProduct(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
         }
     }
 }
